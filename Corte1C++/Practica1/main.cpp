a4183da717e45897b9dc94436f10b7e4df6b6f80
JeffersonAriza117 <arizajeffersonandrei@gmail.com>
2025-09-15 14:48:10 -0500

    Add files via upload

---
+++ Corte1C++/Practica1/main.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop 
+holamundo estoy practicando programacion
+
+*/
+
+//I want to learn to program in C and C++
+
+int main(int argc, char** argv) {
+	
+	std::cout << "hola mundo" << std::endl;
+	
+	return 0;
