156071f0b1e7e23a0b489aa293529c75a6954f62
JeffersonAriza117 <arizajeffersonandrei@gmail.com>
2025-09-29 21:51:28 -0500

    Add files via upload

---
+++ Corte1C++/Practica6/main (1).cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	
+	int i=0;
+	
+	inicio:
+		
+		if(i<10){
+			
+			printf("i= %d\n", i);
+			
+			i++;
+			
+			goto inicio;
+			
+		}
+	
+	return 0;
+	
+}
