c50a1757b0b6addbf119e76d8e381321f9148501
JeffersonAriza117 <arizajeffersonandrei@gmail.com>
2025-10-21 17:12:01 -0500

    Create main.c

---
+++ Corte1C/Practica9/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	
+	int Num = 0;
+	
+	do {
+		
+		printf("Valor de Num = %d\n", Num);
+		
+		Num++;
+		
+	} while (Num<5);
+	
+	return 0;
+}
