0852e90aced4925e58441f70ee0556bffac95478
JeffersonAriza117 <arizajeffersonandrei@gmail.com>
2025-10-21 16:17:27 -0500

    Add files via upload

---
+++ Corte1C++/Practica10/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main() {
+	
+	int myNumbers[] = {25, 50, 75, 100};
+	
+	printf("%d", myNumbers[0]); // empieza a contar desde 0: primer elemento = 0, segundo elemento = 1, tercer elemento = 2...
+	
+	return 0;
+}
